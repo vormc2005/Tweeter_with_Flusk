eb935c70d267b80fce4a4aab18302075d6fc3258
Dmitry Voronov <voronov.dmitry@gmail.com>
2020-06-30 12:29:08 -0400

    sentiment analysis of tweets

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-from flask import Flask, render_template, session, redirect, request, url_for
+from flask import Flask, render_template, session, redirect, request, url_for, g
 from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
 from user import User
 from database import Database
+import requests
 
 app = Flask(__name__)
 app.secret_key = '1234'
 Database.initialize(user='postgres', password='Mysql#123', host='localhost', database='learning')
 
 
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db_by_screen_name(session['screen_name'])
+
+
 @app.route('/')  # http://127.0.0.1:4995
 def homepage():
     return render_template('home.html')
+    # return redirect('login/twitter')
 
 
 @app.route('/login/twitter')
 def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for('profile'))
     request_token = get_request_token()
     # Writing token to cookie, session
     session['request_token'] = request_token
 
     return redirect(get_oauth_verifier_url(request_token))
     # redirecting the user to Twitter so they can confirm authorization
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/auth/twitter')  # http://127.0.0.1:4995/auth/twitter?oauth_verifier=123456
@@ -41,7 +57,22 @@ def twitter_auth():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', screen_name=session['screen_name'])
+    # return render_template('profile.html', screen_name=session['screen_name'])
+    return render_template('profile.html', user=g.user)
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    tweets = g.user.twitter_request('https://api.twitter.com/1.1/search/tweets.json?q={}'.format(query))
+
+    tweet_texts = [{'tweet': tweet['text'], 'label': 'neutral'} for tweet in tweets['statuses']]
+    for tweet in tweet_texts:
+        r = requests.post('http://text-processing.com/api/sentiment/', data={'text': tweet['tweet']})
+        json_response = r.json()
+        label = json_response['label']
+        tweet['label'] = label
+    return render_template('search.html', content=tweet_texts)
 
 
 app.run(port=4995, debug=True)
